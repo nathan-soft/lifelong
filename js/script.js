7d235790b6b7e4e2cf8c4b745a93e2738fcc57c5
gurudanito001 <gurudanito001@gmail.com>
2019-08-23 20:15:16 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.querySelector('.email').focus();
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]');
+
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e) => {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
